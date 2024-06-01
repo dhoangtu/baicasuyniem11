d05c6f77781dd5f3baaf897b1384ffef4d797b9d
dhoangtu <dhoangtu@gmail.com>
2024-06-01 10:54:05 +0700

    xuat ban

--- sach/in-nhanh.bat
+++ sach/in-nhanh.bat
@@ -6,7 +6,7 @@ set GEN=".\pdf-generated"
 D:\download\qpdf-10.1.0\bin\qpdf --empty --pages D:\01.lilypond\01.github\baicasuyniem11-lmkimlong\nhac\*.pdf -- nhac.pdf
 pdflatex so-trang-chan-le.tex
 
-D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-trong-a5.pdf so-trang-chan-le.pdf bia-sau-trong-a5.pdf bia-sau-a5.pdf -- baicasuyniem11.pdf
+D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-truoc-a5.pdf bia-trong-a5.pdf so-trang-chan-le.pdf bia-sau-trong-a5.pdf bia-sau-a5.pdf -- baicasuyniem11.pdf
 
 D:\download\qpdf-10.1.0\bin\qpdf --empty --pages bia-trong-a5.pdf nhac.pdf -- nhac-sach.pdf
 
